0a48fcdebb014e29571a4f3edace4bbf9f126322
Nicky <nicollyporfirio12@gmail.com>
2022-07-30 21:18:40 -0300

    added: about me mobile version

--- src/Components/AboutMe/style.js
+++ src/Components/AboutMe/style.js
@@ -1,1 +1,25 @@
+import styled from "styled-components";
 
+export const Container = styled.div`
+  width: 100vw;
+  height: 65vh;
+  display: flex;
+  align-items: center;
+
+  .boxContent {
+    width: 75%;
+    height: 75%;
+    margin-left: 40px;
+    max-width: 310px;
+  }
+
+  img {
+    width: 25px;
+  }
+
+  p {
+    margin-top: 25px;
+  }
+`;
+
+export const Block = styled.div``;
